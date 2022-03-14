68fd181f939acc9030966b589859a5f36f24b065
Gabriel <gabroham@gmail.com>
2022-03-14 14:38:48 -0700

    Add Player class

---
+++ app/src/main/java/backgammon/Player.java
@@ -0,0 +1,77 @@
+package backgammon;
+
+import java.util.List;
+
+public class Player {
+
+  // Black == 1; White == 2
+  private int color;
+  private String name;
+
+  public Player() {
+    color = 0;
+    name = null;
+  }
+
+  public Player(int color, String name) {
+    this.color = color;
+    this.name = name;
+  }
+
+  public int getColor() {
+    return color;
+  }
+
+  public void setColor(int color) {
+    this.color = color;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  //Selects chip from moveList given
+  public int selectChip(int[] board, List<String> moveList, int moveChoice) {
+    //getting number that the move starts at so we can select the chip at this place
+    char charChip = (moveList.get(moveChoice)).charAt(0);
+    int chip = Character.getNumericValue(charChip);
+
+    //returns the index of the selected chips placement on the board
+    return (board.length - chip);
+  }
+
+  //Finalizes move to be played and updates the board
+  public int[] playMove(int[] board, List<String> moveList, int moveChoice) {
+    //Splits String from moveChoice into an array of landing points
+    String move = moveList.get(moveChoice);
+    String [] points = move.split("-");
+
+    //Number that the chip moves from
+    int startPoint = Integer.parseInt(points[0]);
+
+    //Index of the first point that the chip is moving from
+    int startIndex = board.length - startPoint;
+
+    //Number that the chip moves to
+    int endPoint = Integer.parseInt(points[points.length - 1]);
+
+    //Index of the point the chip moves to
+    int endIndex = board.length - endPoint;
+
+    if(endPoint == 0)
+      board[startIndex] = (board[startIndex] - 1);
+    else {
+      board[startIndex] = (board[startIndex] - 1);
+      board[endIndex] = (board[endIndex] + 1);
+    }
+
+    return board;
+
+
+  }
+}
+
