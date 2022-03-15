e5c71d0af5e6886c9cdd8043ee036c324d34b3a6
Gabriel <gabroham@gmail.com>
2022-03-14 20:35:00 -0700

    Override selectMove in Computer class

--- app/src/main/java/backgammon/Computer.java
+++ app/src/main/java/backgammon/Computer.java
@@ -4,40 +4,10 @@ import java.util.Random;
 
 public class Computer extends Player{
 
-  public String selectMove(List<String> moveList) {
+  @Override
+  public int selectMove() {
     Random rnd = new Random();
-    int choice = rnd.nextInt(moveList.size());
-    return moveList.get(choice);
+    return rnd.nextInt(getMoveList().size());
   }
-
-  //Finalizes move to be played and updates the board
-  public int[] playMove(int[] board, List<String> moveList) {
-    //Splits String from random moveChoice into an array of landing points
-    String move = this.selectMove(moveList);
-    String [] points = move.split("-");
-
-    //Number that the chip moves from
-    int startPoint = Integer.parseInt(points[0]);
-
-    //Index of the first point that the chip is moving from
-    int startIndex = board.length - startPoint;
-
-    //Number that the chip moves to
-    int endPoint = Integer.parseInt(points[points.length - 1]);
-
-    //Index of the point the chip moves to
-    int endIndex = board.length - endPoint;
-
-    if(endPoint == 0)
-      board[startIndex] = (board[startIndex] - 1);
-    else {
-      board[startIndex] = (board[startIndex] - 1);
-      board[endIndex] = (board[endIndex] + 1);
-    }
-
-    return board;
-
-  }
-
 }
 
