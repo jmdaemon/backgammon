d87379971d3e9b5d0b3f120117b581ba1ffdddbb
deep_ <deep_@M2>
2022-03-15 04:06:21 -0700

    Removed empty lines/unwanted imports and variables

--- app/src/main/java/backgammon/Die.java
+++ app/src/main/java/backgammon/Die.java
@@ -1,29 +1,14 @@
 package backgammon;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class Die {
-	
-	
-	private boolean dieDouble;
 
-	//only stores 2 die
-	private int die;	
+	private int die;
 
-	
-
-	//returns the die array
 	public int getRoll() {
 		Random rand = new Random();
-		die = rand.nextInt(6)+1 ;
+		die = rand.nextInt(6) + 1;
 		return die;
-		
 	}
-	
-
-
-	
-
-
 }
