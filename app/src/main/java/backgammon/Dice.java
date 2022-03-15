b4284aec5fe73915a1d88022acfe1fdf42a8b8bb
Joseph Diza <josephm.diza@gmail.com>
2022-03-15 12:08:15 -0700

    Remove dead code

--- app/src/main/java/backgammon/Dice.java
+++ app/src/main/java/backgammon/Dice.java
@@ -8,17 +8,6 @@ public class Dice {
 		dice = new int[2];
 	}
 
-	//public int[] getRolls() {
-		//Die local = new Die();
-		//dice[0] = local.getRoll();
-		//dice[1] = local.getRoll();
-		//dieDouble = false;
-		//if (dice[0] == dice[1]) {
-			//dieDouble = true;
-		//}
-		//return dice;
-	//}
-
 	// Getters
 	public int getFirst() { return dice[0]; }
 	public int getSecond() { return dice[1]; }
@@ -27,22 +16,4 @@ public class Dice {
 	public boolean isDoubles() {
 		return (dice[0] == dice[1]) ? true : false;
 	}
-
 }
-
-//Below is another way if we use ArrayList rather than array
-//
-//	private List<Integer> dice2 = new ArrayList<>();
-//
-//	public List<Integer> getRolls2() {
-//	Die local = new Die();
-//
-//	dice2.add(local.getRoll());
-//	dice2.add(local.getRoll());
-//	dieDouble = false;
-//
-//	if (dice2.get(0).equals(dice2.get(1))) {
-//		dieDouble = true;
-//	}
-//	return dice2;
-//}
