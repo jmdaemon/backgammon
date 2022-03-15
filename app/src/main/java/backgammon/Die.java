6ee80a8b0332fb3732a8ef53b39f9a9bafa57c9f
deep_ <deep_@M2>
2022-03-15 03:24:08 -0700

    Removed unwanted methods and made it so getRoll() return value of 1 die

--- app/src/main/java/backgammon/Die.java
+++ app/src/main/java/backgammon/Die.java
@@ -6,53 +6,23 @@ import java.util.Random;
 public class Die {
 	
 	
-	private boolean dieDouble = false;
+	private boolean dieDouble;
 
 	//only stores 2 die
-	private int[] die = new int[2];
-	
-	//assuming we set it so that the HUMAN player calls roll 
-	public void roll() {
-		Random rand = new Random();
-		die[0] = rand.nextInt(6)+1 ;
-		die[1] = rand.nextInt(6)+1 ;
-		
-		if (die[1] == (die[0]) ) {
-			dieDouble = true;
-		}
-	}
+	private int die;	
+
 	
 
 	//returns the die array
-	public int[] getRoll() {
+	public int getRoll() {
+		Random rand = new Random();
+		die = rand.nextInt(6)+1 ;
 		return die;
 		
 	}
 	
-	public int getFirstDie() {
-		return die[0];
-	}
 
-	public int getSecondDie() {
-		return die[1];
-	}
-	
-	public boolean isDoubles() {
-		return dieDouble;
-	}
-	
 
-	
-	//Just to test 
-	public static void main(String[] args) {
-		Die x = new Die();
-		x.roll();
-		System.out.println(x.getFirstDie());
-		System.out.println(x.getSecondDie());
-		System.out.println(Arrays.toString(x.getRoll()));
-		System.out.println(x.isDoubles());
-		
-	}
 	
 
 
