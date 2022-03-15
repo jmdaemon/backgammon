6e909ef1087e2d04e237a2c3f47bf93bc12cdf0e
Joseph Diza <josephm.diza@gmail.com>
2022-03-14 17:01:05 -0700

    Resolve inconsistent use of ArraysList and Arrays

--- app/src/test/java/backgammon/MoveletTests.java
+++ app/src/test/java/backgammon/MoveletTests.java
@@ -3,31 +3,32 @@ package backgammon;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class MoveletTests {
+
   @Test
   public void canConstructMoveletsForNonDoubles() {
-    List<Integer> dice = new ArrayList<>(Arrays.asList(3, 5));
+    List<Integer> dice = Arrays.asList(3, 5);
     Move m = new Move(2, 10, 1, dice, false);
     List<Movelet> result = m.getMovelets();
-    List<Movelet> expected = new ArrayList<>();
-    expected.add(new Movelet(2, 5, 3));
-    expected.add(new Movelet(5, 10, 5));
+    List<Movelet> expected = Arrays.asList(
+        new Movelet(2, 5, 3),
+        new Movelet(5, 10, 5));
     //You'll need to add your checkResult thing
   }
 
   @Test
   public void canConstructMoveletsForDoubles() {
-    List<Integer> dice = new ArrayList<>(Arrays.asList(2, 2, 2, 2));
+    List<Integer> dice = Arrays.asList(2, 2, 2, 2);
     Move m = new Move(2, 10, 1, dice, true);
     List<Movelet> result = m.getMovelets();
-    List<Movelet> expected = new ArrayList<>();
-    expected.add(new Movelet(2, 4, 2));
-    expected.add(new Movelet(4, 6, 2));
-    expected.add(new Movelet(6, 8, 2));
-    expected.add(new Movelet(8, 10, 2));
+    List<Movelet> expected = Arrays.asList(
+        new Movelet(2, 4, 2),
+        new Movelet(4, 6, 2),
+        new Movelet(6, 8, 2),
+        new Movelet(8, 10, 2));
     //You'll need to add your checkResult thing x2
   }
 }
